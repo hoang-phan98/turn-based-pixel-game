b4614aa19e05555729a48613aa88d101951c72ec
Hoang Phan <hpha0005@student.monash.edu>
2019-05-07 09:26:21 +1000

    Adding another map location

--- src/game/Application.java
+++ src/game/Application.java
@@ -8,6 +8,7 @@ import edu.monash.fit2099.engine.Display;
 import edu.monash.fit2099.engine.FancyGroundFactory;
 import edu.monash.fit2099.engine.GameMap;
 import edu.monash.fit2099.engine.Item;
+import edu.monash.fit2099.engine.MoveActorAction;
 import edu.monash.fit2099.engine.Player;
 import edu.monash.fit2099.engine.World;
 
@@ -34,16 +35,36 @@ public class Application {
 		gameMap = new GameMap(groundFactory, map);
 		world.addMap(gameMap);
 		
+        List<String> map2 = Arrays.asList(
+				".......................",
+				".......................",
+				".......................",
+				".......................",
+				".......................",
+				".......................",
+				".......................",
+				".......................",
+				".......................",
+				".......................",
+				".......................");
+        
+        GameMap gameMap2 = new GameMap(groundFactory, map2);
+        world.addMap(gameMap2);
+
+        Item rocket = Item.newFurniture("Rocket", '>');
+        rocket.getAllowableActions().add(new MoveActorAction(gameMap2.at(10, 5), "to Map 2!"));
+        gameMap.addItem(rocket, 22, 5);
+        
+        Item rocket2 = Item.newFurniture("Rocket", '<');
+        rocket2.getAllowableActions().add(new MoveActorAction(gameMap.at(22, 5), "to Map 1!"));
+        gameMap2.addItem(rocket2, 0, 5);
+		
 		Actor player = new Player("Player", '@', 1, 100);
 		world.addPlayer(player, gameMap, 5, 10);
 		
-		Item key = new Key("key", 'k');
-		
 		Grunt grunt = new Grunt("Mongo", player);
-		grunt.addItemToInventory(key);
 		gameMap.addActor(grunt, 0, 0);
 		Grunt grunt2 = new Grunt("Norbert", player);
-		grunt2.addItemToInventory(key);
 		gameMap.addActor(grunt2,  10, 10);
 		Goon goon = new Goon("Heckler", player);
 		goon.addItemToInventory(key);
