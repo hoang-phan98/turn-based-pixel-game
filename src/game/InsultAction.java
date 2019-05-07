1f6d370a9bb491b934bfeedd89a3b48a14bd258b
Josie Leye <jiley1@student.monash.edu>
2019-05-07 17:09:10 +1000

    Randomize Insults

--- src/game/InsultAction.java
+++ src/game/InsultAction.java
@@ -1,6 +1,9 @@
 package game;
 
 import java.util.Random;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
 
 import edu.monash.fit2099.engine.Action;
 import edu.monash.fit2099.engine.Actor;
@@ -12,8 +15,13 @@ import edu.monash.fit2099.engine.Weapon;
 public class InsultAction extends Action {
 	private Actor actor;
 	private Actor target;
-	private Random rand = new Random();
-//	private List listOfInsults = new 
+	private Random rand= new Random();
+	private Double chance = 100*rand.nextDouble();
+	private String insult;
+	private String result;
+	private String[] listOfInsults = {"You will never succeed","You've gained weight","Your hairline is receeding"};
+	
+//	private List<String> listOfInsults = new ArrayList<String>();
 
 	public InsultAction(Actor actor, Actor target) {
 		this.actor = actor;
@@ -24,13 +32,27 @@ public class InsultAction extends Action {
 	public String execute(Actor actor, GameMap map) {
 
 		Weapon weapon = actor.getWeapon();
+		int damage = weapon.damage();
+		
 
 		if (rand.nextBoolean()) {
 			return actor + " misses " + target + ".";
 		}
+		if (chance<30) {
+			result = actor + " yells " + listOfInsults[0] + " and " +weapon.verb() + " " + target + " for " + damage + " damage.";
+		}
+		else if (chance<60) {
+			result = actor + " yells " + listOfInsults[1] + " and " +weapon.verb() + " " + target + " for " + damage + " damage.";
+		}
+		else if (chance<90) {
+			result = actor + " yells " + listOfInsults[2] + " and " +weapon.verb() + " " + target + " for " + damage + " damage.";
+		}
+		else {
+			result = actor + " " + weapon.verb() + " " + target + " for " + damage + " damage.";
+		}
 
-		int damage = weapon.damage();
-		String result = actor + " " + weapon.verb() + " " + target + " for " + damage + " damage.";
+		
+//		String result = actor + " " + weapon.verb() + " " + target + " for " + damage + " damage.";
 
 		target.hurt(damage);
 		if (!target.isConscious()) {
