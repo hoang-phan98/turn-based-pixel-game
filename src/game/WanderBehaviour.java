8221d5939921c0b87ff28ea868acb2c751c74f1e
Hoang Phan <hpha0005@student.monash.edu>
2019-05-08 17:35:25 +1000

    Randomize Q's movement

--- src/game/WanderBehaviour.java
+++ src/game/WanderBehaviour.java
@@ -1,5 +1,8 @@
 package game;
 
+import java.util.ArrayList;
+import java.util.Random;
+
 import edu.monash.fit2099.engine.Action;
 import edu.monash.fit2099.engine.Actor;
 import edu.monash.fit2099.engine.Exit;
@@ -9,16 +12,30 @@ import edu.monash.fit2099.engine.MoveActorAction;
 import edu.monash.fit2099.engine.SkipTurnAction;
 
 public class WanderBehaviour implements ActionFactory {
-
+	private Random rand = new Random();
+	private ArrayList<Exit> exits = new ArrayList<Exit>();
+	
 	@Override
 	public Action getAction(Actor actor, GameMap map) {
 		Location here = map.locationOf(actor);
 		for (Exit exit : here.getExits()) {
-			Location destination = exit.getDestination();
-			if (destination.canActorEnter(actor)) {
-				return new MoveActorAction(destination, exit.getName());	
-			}
+			exits.add(exit);
 		}
+		
+		Exit exit = exits.get(rand.nextInt(exits.size()));
+		Location destination = exit.getDestination();
+		
+		int counter = 0;
+		while(!destination.canActorEnter(actor) && counter < exits.size()) {
+			exit = exits.get(rand.nextInt(exits.size()));
+			destination = exit.getDestination();
+			counter++;
+		}
+		
+		if (destination.canActorEnter(actor)) {
+			return new MoveActorAction(destination, exit.getName());	
+		}
+		
 		return new SkipTurnAction();
 	}
 
