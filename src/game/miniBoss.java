582aac528b43d32def08a54916d67fca6c968930
Josie Leye <jiley1@student.monash.edu>
2019-05-08 11:44:17 +1000

    Update to RocketEngine and MiniBoss

--- src/game/miniBoss.java
+++ src/game/miniBoss.java
@@ -1,12 +1,39 @@
 package game;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import edu.monash.fit2099.engine.*;
 
 public class miniBoss extends Actor {
 	
 	public miniBoss(String name) {
 		super(name, 'd', 5, 10);
-		Item key = new Key("key", 'k');
-		this.addItemToInventory(key);
+		Item engine = new RocketEngine("Rocket Engine", 'e');
+		this.addItemToInventory(engine);
+		
+	}
+	private List<ActionFactory> actionFactories = new ArrayList<ActionFactory>();
+
+	private void addBehaviour(ActionFactory behaviour) {
+		actionFactories.add(behaviour);
+	}
+	
+	
+	@Override
+	protected IntrinsicWeapon getIntrinsicWeapon() {
+		return new IntrinsicWeapon(1, "lightly slaps");
+	}
+
+	@Override
+	public Action playTurn(Actions actions, GameMap map, Display display) {
+		for (ActionFactory factory : actionFactories) {
+			Action action = factory.getAction(this, map);
+			if(action != null)
+				return action;
+		}
+		
+		return super.playTurn(actions,  map,  display);
 	}
 }
+
