dd15b5bd348bc86e7212aeab7432f7f2a9e1aec8
Josie Leye <jiley1@student.monash.edu>
2019-05-09 21:25:44 +1000

    Javadoc and updating hitpoints

--- src/game/RocketEngine.java
+++ src/game/RocketEngine.java
@@ -4,6 +4,12 @@ import edu.monash.fit2099.engine.Actions;
 import edu.monash.fit2099.engine.Item;
 import edu.monash.fit2099.engine.DropItemAction;
 
+/**
+ * An Item that will be needed for BuildRocketAction. 
+ * Only Doctor Maybe will have a RocketEngine in his inventory
+ * at the start of the game.
+ * Can be picked up my any player
+ */
 public class RocketEngine extends Item {
 	public RocketEngine(String name, char displayChar) {
 		super(name, displayChar);
