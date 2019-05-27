5e282934d675df035c2b679b56438b7c6c7b8ad2
Josie Leye <jiley1@student.monash.edu>
2019-05-27 21:39:30 +1000

    Adding Oxygen points to tank
    Methods to use oxygen and check oxygen

--- src/game/OxygenTank.java
+++ src/game/OxygenTank.java
@@ -3,9 +3,21 @@ package game;
 import edu.monash.fit2099.engine.Item; 
  
 public class OxygenTank extends Item { 
- 
+	private int oxygenPoints = 10;
+	
 	public OxygenTank() { 
 		super("Oxygen Tank", 'T'); 
 	} 
+	
+	public void useOxygen() {
+		this.oxygenPoints  -= 1;
+	}
+	
+	public boolean isEmpty() {
+		if (this.oxygenPoints == 0) {
+			return true;
+		}
+		return false;
+	}
 	 
 } 
