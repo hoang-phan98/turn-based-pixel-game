323bc639e52e4a0c0731ba5327cd71315dd352b9
Josie Leye <jiley1@student.monash.edu>
2019-05-31 01:35:00 +1000

    Change BuildRocket to check Actor Items not location items

--- src/game/BuildRocketAction.java
+++ src/game/BuildRocketAction.java
@@ -32,16 +32,18 @@ public class BuildRocketAction extends Action {
 	public String execute(Actor actor, GameMap map) {
 		Item rocket = new Rocket();
 		rocket.getAllowableActions().add(new MoveActorAction(mapTo.at(location.x(), location.y()), " to the Moon!"));
-		
+					
 		boolean hasBody = false;
 		boolean hasEngine = false;
 		
-		for(Item item: map.locationOf(actor).getItems()) {
+		for(Item item: actor.getInventory()) {
 			if(item instanceof RocketBody) {
 				hasBody = true;
+				actor.removeItemFromInventory(item);
 			}
 			if(item instanceof RocketEngine) {
 				hasEngine = true;
+				actor.removeItemFromInventory(item);
 			}
 		}
 		
