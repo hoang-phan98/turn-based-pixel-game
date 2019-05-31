abaee8dbae8e57b85fa39a5bd83a8b0b0ce822b7
Hoang Phan <hpha0005@student.monash.edu>
2019-05-31 23:16:57 +1000

    Making Goons an extension of Grunts

--- src/game/Goon.java
+++ src/game/Goon.java
@@ -1,34 +1,17 @@
 package game;
 
-import java.util.ArrayList;
-import java.util.List;
-import java.util.Random;
-
 import edu.monash.fit2099.engine.*;
 
 /**
- * Represents a class of hostile actors towards the player
+ * Represents a class of hostile actors towards the player, extended from Grunts that 
+ * deals twice the damage
  */
-public class Goon extends Actor {
-	private Random rand = new Random();
-	private Actor target;
-	
+public class Goon extends Grunt {
 	/**
-	 * Goons will have 10hp and is represented with an 'o'
+	 * Goons will have the same hp as Grunts and will also be represented with a 'g'
 	 */
-	
 	public Goon(String name, Actor player) {
-		super(name, 'o', 5, 10);
-		Item key = new Key();
-		this.addItemToInventory(key);
-		addBehaviour(new FollowBehaviour(player));
-		this.target = player;
-	}
-
-	private List<ActionFactory> actionFactories = new ArrayList<ActionFactory>();
-
-	private void addBehaviour(ActionFactory behaviour) {
-		actionFactories.add(behaviour);
+		super(name, player);
 	}
 
 	/**
@@ -39,28 +22,4 @@ public class Goon extends Actor {
 		return new IntrinsicWeapon(8, "hits");
 	}
 
-	@Override
-	/**
-	 * Goons will always try to follow the player first. If the are next to them,
-	 * then they will perform a random allowed action other than DropItem and 
-	 * PickUpItem.
-	 */
-	public Action playTurn(Actions actions, GameMap map, Display display) {
-		int ranInt = rand.nextInt(10);
-		if(ranInt < 1) {
-			return new InsultAction(target);
-		}
-		for (ActionFactory factory : actionFactories) {
-			Action action = factory.getAction(this, map);
-			if(action != null)
-				return action;
-		}
-		
-		Action action = new DropItemAction(null);
-		while(action instanceof DropItemAction || action instanceof PickUpItemAction || action instanceof UnlockDoorAction) {
-			action = actions.get(rand.nextInt(actions.size()));
-		}
-		
-		return action;
-	}
 }
